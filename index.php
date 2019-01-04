319fd5a2356a4b765dd5c60e4230c90230ec5337
kestasgim <kestutis.gimbutas@gmail.com>
2019-01-04 15:36:52 +0200

    Add prepos, requirements
    Add requirements in index.html,  begin using prepos

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <h1>INDEX</h1>
+<div>
+	<ul>
+		<h4>Keli vertinimo kriterijai:</h4>
+		<li>- kodo kultura (atitraukimai, aiskus klasiu pavadinimai)</li>
+		<li>- validus HTML</li>
+		<li>- validus CSS</li>
+		<li>- CSS good practice (pvz. nenaudoti statiniu auksciu, nenaudoti statiniu plociu kur jie nereikalingi)</li>
+		<li>- CSS neturi pasikartojanciu taisykliu. (DRY metodologija)</li>
+		<li>- 4-5 Skirtingo isdestymo blokai puslapyje</li>
+		<li>- Responsive</li>
+		<li>- WP visas svetaines turinys administruojamas</li>
+		<li>- WP admin (kaip organizuotas duomenu ivedimas ar lankstus ar ne)</li>
+		<li>- WP sablono kodo kultura (pagrinde atitraukimai)</li>
+	</ul>
+</div>
